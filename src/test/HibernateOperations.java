a50f6362586bd9b5412b634250d5f7b83c60ad47
Gaetano Vitale <givitale@exprivia.local>
2018-09-06 10:40:34 +0200

    Commit Gaetano

---
+++ src/test/HibernateOperations.java
@@ -0,0 +1,46 @@
+package test;
+
+import java.util.List;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import exprivia.Dlfileentry;
+
+public class HibernateOperations {
+	
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		
+		@SuppressWarnings("deprecation")
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		try {
+			
+			SQLQuery sqlQuery = session.createSQLQuery("SELECT * "
+					+ "FROM lportal.dlfileentry "
+					+ "WHERE dlfileentry.createDate >= date(curdate() - interval 3 day) "
+					+ "+ interval 0 second;"); 
+			sqlQuery.addEntity(Dlfileentry.class);
+			@SuppressWarnings("unchecked")
+			List<Dlfileentry> entries = sqlQuery.list();
+			System.out.println("Number of Dlfileentries: " + entries.size());
+			for(Dlfileentry dlfileentry: entries){
+				System.out.println("Title: " + dlfileentry.getTitle());
+				System.out.println("UserName " + dlfileentry.getUserName());
+				System.out.println("CreateDate " + dlfileentry.getCreateDate());
+				System.out.println("=========================================");
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			session.close();
+		}
+
+	}
+
+}
